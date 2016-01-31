e26915a08a16cf5f972662b76877ebe7d9b904b9
Oleksandr Boiko <nezhraba@gmail.com>
2016-01-31 22:41:41 +0200

    LanguageRepository::getAll

--- src/AppBundle/Entity/AbstractRepository.php
+++ src/AppBundle/Entity/AbstractRepository.php
@@ -2,159 +2,56 @@
 
 namespace AppBundle\Entity;
 
-use Exception;
-use mysqli;
-use Symfony\Component\Routing\Exception\MissingMandatoryParametersException;
-
 abstract class AbstractRepository
 {
     /** @var Identifiable[] */
     private $entities = [];
 
     /**
-     * @return string
+     * @param array $data
+     * @return Identifiable
      */
-    abstract protected function getEntityClass();
-
-    /**
-     * return string[] [$fieldName, ]
-     */
-    protected function constructorMapping()
-    {
-        return [];
-    }
-
-    /**
-     * @return string[] [$fieldName => $setterMethodName, ]
-     */
-    protected function settersMapping()
-    {
-        return [];
-    }
+    abstract protected function createByData(array $data);
 
     /**
      * @param array $data
      * @return Identifiable
      */
-    private function createByData(array $data)
+    protected function getByData(array $data)
     {
-        $constructorArgs = [];
-        foreach ($this->constructorMapping() as $fieldName) {
-            $constructorArgs[] =
-                $this->getMandatoryFieldValue($data, $fieldName);
-        }
+        return $this->getByIdData($data['id'], $data);
+    }
 
-        /** @var Identifiable $entity */
-        $entity = (new \ReflectionClass($this->getEntityClass()))
-            ->newInstanceArgs($constructorArgs);
-        foreach ($this->settersMapping() as $fieldName => $setter) {
-            $entity->$setter($this->getMandatoryFieldValue($data, $fieldName));
+    /**
+     * @param int $id
+     * @param array $data
+     * @return Identifiable
+     */
+    protected function getByIdData($id, array $data)
+    {
+        if (isset($this->entities[$id])) {
+            return $this->entities[$id];
         }
-
-        $this->keep($entity);
+        $entity = $this->createByData($data);
+        $this->entities[$entity->getId()] = $entity;
 
         return $entity;
     }
 
     /**
-     * @param string $query
-     * @param array $params
-     * @param int|null $expectedAmount
-     * @return Identifiable[]
-     * @throws Exception
+     * @param array $array
+     * @param string[] $fields
+     * @throws \Exception
      */
-    protected function getManyByQuery(
-        $query,
-        array $params = [],
-        $expectedAmount = null
-    ) {
-        $entities = [];
-        foreach ($this->getSqlDataByQuery($query, $params) as $row) {
-            $entities[] = $this->createByData($row);
-        }
-        if ($expectedAmount !== null && count($entities) !== $expectedAmount) {
-            throw new Exception(sprintf(
-                '%d %s objects were generated instead of %d',
-                count($entities),
-                $this->getEntityClass(),
-                $expectedAmount
-            ));
-        }
-        return $entities;
-    }
-
-    /**
-     * @param string $query
-     * @param array $params
-     * @return Identifiable
-     * @throws Exception
-     */
-    protected function getOneByQuery($query, array $params = [])
+    protected function mandatory(array $array, array $fields)
     {
-        $entities = $this->getManyByQuery($query, $params);
-        if (count($entities) === 0) {
-            throw new Exception(sprintf(
-                'Can not create %s object',
-                $this->getEntityClass()
-            ));
+        foreach ($fields as $field) {
+            if (!isset($array[$field])) {
+                throw new \Exception(sprintf(
+                    'Missing required field %s',
+                    $field
+                ));
+            }
         }
-        if (count($entities) > 1) {
-            throw new Exception(sprintf(
-                'Multiple %s objects were created, instead of one',
-                $this->getEntityClass()
-            ));
-        }
-        return $entities[0];
-    }
-
-    /**
-     * @param array $data
-     * @param string $fieldName
-     * @return mixed
-     */
-    private function getMandatoryFieldValue(array $data, $fieldName)
-    {
-        if (!isset($data[$fieldName])) {
-            throw new MissingMandatoryParametersException();
-        }
-
-        return $data[$fieldName];
-    }
-
-    /**
-     * @param Identifiable $entity
-     */
-    private function keep(Identifiable $entity)
-    {
-        $this->entities[$entity->getId()] = $entity;
-    }
-
-    /**
-     * @param string $query
-     * @param array $params
-     * @return string[][]
-     */
-    private function getSqlDataByQuery($query, array $params = [])
-    {
-        $mysqli=new mysqli();
-        $mysqli->connect('127.0.0.1','root','','parallango');
-        $mysqli->query('SET NAMES utf8');
-
-        foreach ($params as $paramName => $paramValue) {
-            $query = str_replace(
-                ':' . $paramName,
-                '\'' . str_replace(["\\"], ["\\\\"], $paramValue) . '\'',
-                $query
-            );
-        }
-
-        $data = [];
-        $res = $mysqli->query($query);
-        while ($row = mysqli_fetch_assoc($res)) {
-            $data[] = $row;
-        }
-
-        $mysqli->close();
-        return $data;
     }
 }
---
+++ src/AppBundle/Entity/Language/LanguageRepository.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace AppBundle\Entity\Language;
+
+use AppBundle\Entity\AbstractRepository;
+use AppBundle\Utils\DB\SQL;
+
+class LanguageRepository extends AbstractRepository
+{
+    /** @var SQL */
+    private $sql;
+
+    /**
+     * @param SQL $sql
+     */
+    public function __construct(SQL $sql)
+    {
+        $this->sql = $sql;
+    }
+
+    /**
+     * @param string $code
+     * @return Language
+     */
+    public function getByCode($code)
+    {
+        return $this->getByData($this->sql->getRow(
+            <<<'SQL'
+            SELECT id, code
+            FROM languages
+            WHERE code = :code
+SQL
+            ,
+            ['code' => $code,]
+        ));
+    }
+
+    /**
+     * @return Language[]
+     */
+    public function getAll()
+    {
+        $rows = $this->sql->getArray(
+            <<<'SQL'
+            SELECT id, code
+            FROM languages
+SQL
+        );
+        $languages = [];
+        foreach ($rows as $row) {
+            $languages[] = $this->getByData($row);
+        }
+        return $languages;
+    }
+
+    /**
+     * @param array $data
+     * @return Language
+     */
+    protected function createByData(array $data)
+    {
+        $this->mandatory($data, ['id', 'code']);
+        return new Language($data['id'], $data['code']);
+    }
+}
