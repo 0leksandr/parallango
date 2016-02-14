1ea958f1ff160561950d4f7fd367312538075bf6
Oleksandr Boiko <nezhraba@gmail.com>
2016-02-14 10:26:27 +0200

    Foreign keys test

--- tests/DBTest.php
+++ tests/DBTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use Utils\ServiceContainer;
+
 class DBTest extends PHPUnit_Framework_TestCase
 {
     /**
@@ -7,6 +9,43 @@ class DBTest extends PHPUnit_Framework_TestCase
      */
     public function checkForeignKeys()
     {
-        $this->assertTrue(false);
+        $sql = ServiceContainer::get('test')->get('sql');
+        foreach ([
+            'author_language_properties' => [
+                'author_id' => 'authors.id',
+                'language_id' => 'languages.id',
+                'property_id' => 'author_language_property.id',
+            ],
+            'section_titles' => [
+                'section_id' => 'sections.id',
+                'language_id' => 'languages.id',
+            ],
+        ] as $table => $columns) {
+            foreach ($columns as $column => $foreignColumn) {
+                list($foreignTable, $foreignColumn) =
+                    explode('.', $foreignColumn);
+                $thisValues = $sql->getColumn("
+                    SELECT $column
+                    FROM $table
+                ");
+                $foreignValues = $sql->getColumn("
+                    SELECT $foreignColumn
+                    FROM $foreignTable
+                ");
+                $nonPresent = array_diff($thisValues, $foreignValues);
+                $this->assertEquals(
+                    0,
+                    count($nonPresent),
+                    sprintf(
+                        '%s.%s has value %s, that is not present in %s.%s',
+                        $table,
+                        $column,
+                        head($nonPresent),
+                        $foreignTable,
+                        $foreignColumn
+                    )
+                );
+            }
+        }
     }
 }
