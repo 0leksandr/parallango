eb24f2ab428cee3c0ca67780509acc2123ee6863
Oleksandr Boiko <nezhraba@gmail.com>
2016-08-14 18:02:24 +0300

    Slightly refactored controller test

--- tests/integration/AppBundle/Controller/DefaultControllerTest.php
+++ tests/integration/AppBundle/Controller/DefaultControllerTest.php
@@ -2,21 +2,42 @@
 
 namespace AppBundle\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Client;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\DomCrawler\Crawler;
 
 class DefaultControllerTest extends WebTestCase
 {
+    /** @var Client */
+    private $client;
+
+    public function setUp()
+    {
+        $this->client = static::createClient();
+    }
+
+    /**
+     * @test
+     */
     public function testIndex()
     {
-        $client = static::createClient();
+        $crawler = $this->getCrawler('/');
 
-        $crawler = $client->request('GET', '/');
-
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains(
             'Learn English by reading books.',
             $crawler->filter('title')->text()
         );
         $this->assertContains('[Alpha]', $crawler->filter('#alpha')->text());
+    }
+
+    /**
+     * @param string $uri
+     * @return Crawler
+     */
+    private function getCrawler($uri)
+    {
+        $crawler = $this->client->request('GET', $uri);
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+        return $crawler;
     }
 }
